699cad17442458edb0e3c64b4fe1b1c4955e80fe
c0st1nus <Koshevoy_K8030@kst.nis.edu.kz>
2023-10-21 00:48:18 +0600

    Добавил регистрацию

--- Bot.py
+++ Bot.py
@@ -1,41 +1,83 @@
+import json
+import random
+
 import telebot
-import main.processing
+from telebot import types
 
 bot = telebot.TeleBot('6198996168:AAH5oA_YT9n2UZMuKKfVcQmvflk_jm-zRhM')
-chatID = '1239398217'
+chatID = None
 
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    global ChatID
-    bot.send_message(message.chat.id, f'Бот начал работу')
-    main.processing.loop()
 
 def debug(message):
-    print(chatID)
     textFile = open('Changes.txt', mode='a')
     textFile.write(message + '\n')
     textFile.close()
 
 
+# noinspection PyTypeChecker
 def send():
-    if chatID is not None:
-        textfile = open('Changes.txt', mode='r')
-        text = textfile.read()
+    global chatID
+    textfile = open('Changes.txt', mode='r')
+    text = textfile.read()
+    textfile.close()
+    if text:
+        global chatID
+        bot.send_message(chatID, text="Бот закончил анализ таблицы")
+        with open('Changes.txt', 'rb') as document:
+            bot.send_document(chatID, document)
+    else:
+        textfile = open('Changes.txt', mode='a')
+        textfile.write('Нет изменений')
         textfile.close()
+        bot.send_message(chatID, text="Бот закончил анализ таблицы")
+        with open('Changes.txt', 'rb') as document:
+            bot.send_document(chatID, document)
+    print(chatID)
 
-        if text:
-            bot.send_message(chatID, text="Бот закончил анализ таблицы")
-            with open('Changes.txt', 'rb') as document:
-                bot.send_document(chatID, document)
-        else:
-            textfile = open('Changes.txt', mode='a')
-            textfile.write('Нет изменений')
-            textfile.close()
-            bot.send_message(chatID, text="Бот закончил анализ таблицы")
-            with open('Changes.txt', 'rb') as document:
-                bot.send_document(chatID, document)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.text == '/start':
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton('Зарегистрироваться', callback_data='Reg'),
+                     types.InlineKeyboardButton('Авторизироваться', callback_data='Log'))
+        bot.send_message(message.chat.id, "Здравствуйте! У вас есть аккаунт или вы здесь впервые?", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'Reg':
+        password = randompassword(8)
+        bot.send_message(callback.message.chat.id, text=f'Регистрация успешно выполнена, ваш пароль: {password}')
+        with open('UsersData/Users.json', 'r') as file:
+            data = json.load(file)
+        data['usernames'].append(f'{callback.message.from_user.id}')
+        data['passwords'].append(f'{password}')
+        with open('UsersData/Users.json', 'w') as file:
+            json.dump(data, file)
+    elif callback.data == 'Log':
+        with open('UsersData/Users.json', 'r') as file:
+            data = json.load(file)
+        if f'{callback.message.from_user.id}' in data['usernames']:
+            bot.send_message(callback.message.chat.id, text='Введите ваш пароль')
+            bot.register_next_step_handler(callback.message, login, data['passwords'][data['usernames'].index(f'{callback.message.from_user.id}')])
+
+
+def login(message, realPassword):
+    if realPassword == message.text:
+        bot.send_message(message.chat.id, 'Успешный вход')
+    else:
+        bot.send_message(message.chat.id, "Пароль не верный")
+        bot.send_message(message.chat.id, 'Введите ваш пароль')
+        bot.register_next_step_handler(message, login, realPassword)
+
+
+def randompassword(count):
+    randm = []
+    for i in range(count):
+        randm.append(str(random.randint(0, 10)))
+    sep = ''
+    return sep.join(randm)
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
